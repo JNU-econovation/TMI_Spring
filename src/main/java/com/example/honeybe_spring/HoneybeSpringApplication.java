095f16095732109503e519cf05c4bd3141d74fe2
saint6839 <saint6839@gmail.com>
2021-12-30 20:51:05 +0900

    initial commit

---
+++ src/main/java/com/example/honeybe_spring/HoneybeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.honeybe_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoneybeSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoneybeSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/honeybe_spring/HoneybeSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.honeybe_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneybeSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
